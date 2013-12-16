846719429c40f6b013553e5cf8089fd7338f1fd4
Esteban Valery <evalery@dridco.com>
2013-12-16 15:54:56 -0300

    Ahora funciona

--- javademoapp/src/main/java/com/demoapp/javademoapp/BaseController.java
+++ javademoapp/src/main/java/com/demoapp/javademoapp/BaseController.java
@@ -1,18 +1,21 @@
 package com.demoapp.javademoapp;
  
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+
 @Controller
-@RequestMapping("/")
 public class BaseController {
  
 	@RequestMapping(value="/welcome", method = RequestMethod.GET)
-	public String welcome(ModelMap model) {
- 
-		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+	public String welcome(ModelMap model) throws UnknownHostException {
+		InetAddress address = InetAddress.getLocalHost();
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome() - desde " + address.getHostAddress());
  
 		//Spring uses InternalResourceViewResolver and return back index.jsp
 		return "index";
@@ -20,9 +23,9 @@ public class BaseController {
 	}
  
 	@RequestMapping(value="/welcome/{name}", method = RequestMethod.GET)
-	public String welcomeName(@PathVariable String name, ModelMap model) {
- 
-		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - " + name);
+	public String welcomeName(@PathVariable String name, ModelMap model) throws UnknownHostException {
+		InetAddress address = InetAddress.getLocalHost();
+		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - " + name + " - desde " + address.getHostAddress());
 		return "index";
  
 	}
